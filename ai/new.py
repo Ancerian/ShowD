44e71090ad33a9fae542baa48533ed794aedcd8d
Roman Rader <roman.rader@gmail.com>
2025-05-14 17:29:33 +0300

    Initial

---
+++ ai/new.py
@@ -0,0 +1,58 @@
+import cv2
+import time
+import numpy as np
+
+from tensorflow.lite.python.interpreter import Interpreter
+
+interpreter = Interpreter(model_path="tflite-model/tflite_learn_4.tflite")
+interpreter.allocate_tensors()
+
+print(interpreter.get_input_details())
+print(interpreter.get_output_details())
+
+stream = cv2.VideoCapture(0)
+stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*'MJPG'))
+stream.set(3, 640)
+stream.set(4, 480)
+
+while True:
+    (grabbed, frame) = stream.read()
+    cv2.imshow('Object detector', frame)
+
+    frame = frame.copy()
+    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_resized = cv2.resize(frame_rgb, (96, 96))
+    frame_resized = np.expand_dims(frame_resized, axis=-1)
+    input_data = np.expand_dims(frame_resized, axis=0)
+
+    # Convert to int8 using quantization parameters
+    input_data = (np.round(input_data.astype(np.int32)) - 127)
+    input_data = np.clip(input_data, -128, 127).astype(np.int8)
+
+    interpreter.set_tensor(0, input_data)
+    interpreter.invoke()
+
+    scores = interpreter.get_tensor(180).astype(np.int32).flatten()
+    scores_float = (scores + 128) * 0.003921568859368563
+
+    prob = scores_float / sum(scores_float)
+    print(sum(prob))
+    prob_names = zip([
+        "down",
+        "left",
+        "nothing",
+        "right",
+        "stop",
+        "up"
+    ], prob)
+
+    max_class = max(prob_names, key=lambda x: x[1])
+    print(f"Max class: {max_class[0]} with probability {max_class[1]}")
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+    time.sleep(1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ controllers/cfc/server.py
@@ -0,0 +1,36 @@
+from flask import Flask
+import queue
+
+CONTROL_QUEUE = queue.Queue()
+app = Flask(__name__)
+
+
+@app.route("/up")
+def drone_up():
+    CONTROL_QUEUE.put("UP")
+    return "UP OK"
+
+
+@app.route("/down")
+def drone_down():
+    CONTROL_QUEUE.put("DOWN")
+    return "DOWN OK"
+
+@app.route("/forward")
+def drone_forward():
+    CONTROL_QUEUE.put("FORWARD")
+    return "FORWARD OK"
+
+@app.route("/backward")
+def drone_backward():
+    CONTROL_QUEUE.put("BACKWARD")
+    return "BACKWARD OK"
+
+
+
+def start_server():
+    app.run(host="localhost", port=5000, debug=True, use_reloader=False)
+
+
+if __name__ == "__main__":
+    start_server()
